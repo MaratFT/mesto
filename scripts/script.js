6fc604086b050d3ff49b6118bb93fa31b0611860
Marat Mustafin <mustafin.m-marat@yandex.ru>
2023-03-07 02:10:02 +0400

    fix popup css html

--- scripts/script.js
+++ scripts/script.js
@@ -2,75 +2,39 @@ let page = document.querySelector(".page");
 
 let profileEditButton = page.querySelector(".profile__edit-button");
 
-let closeButton = page.querySelector(".popup__close");
-
-function clickCloseButton() {
-  popup.classList.add("popup_disable");
-}
-
-closeButton.addEventListener("click", clickCloseButton);
+let buttonClose = page.querySelector(".popup__close");
 
 let popup = page.querySelector(".popup");
 
+let titleCopy = page.querySelector(".profile__title");
+
+let subtitleCopy = page.querySelector(".profile__subtitle");
+
+let nameInput = page.querySelector("#user-name-input");
+
+let aboutMeInput = page.querySelector("#about-me-input");
+
+let formSave = page.querySelector(".popup__fields");
+
 function clickEditButton() {
   popup.classList.add("popup_opened");
-  console.log("Открытие попапа");
+  nameInput.value = titleCopy.textContent;
+  aboutMeInput.value = subtitleCopy.textContent;
+}
+
+function saveFormSubmit(evt) {
+  evt.preventDefault();
+  titleCopy.textContent = nameInput.value;
+  subtitleCopy.textContent = aboutMeInput.value;
+  clickCloseButton();
+}
+
+function clickCloseButton() {
+  popup.classList.remove("popup_opened");
 }
 
 profileEditButton.addEventListener("click", clickEditButton);
 
-function clickCloseButton() {
-  popup.classList.remove("popup_opened");
-  console.log("Закрытие попапа");
-  let a = page.querySelector(".profile__title").innerHTML;
-  page.querySelector("#user-name-input").value = a;
-  let b = page.querySelector(".profile__subtitle").innerHTML;
-  page.querySelector("#about-me-input").value = b;
-}
+buttonClose.addEventListener("click", clickCloseButton);
 
-closeButton.addEventListener("click", clickCloseButton);
-
-let buttonSave = page.querySelector(".popup__button-save");
-
-function saveForm() {
-  console.log("Кнопка сохранения работает");
-  let a = page.querySelector("#user-name-input").value;
-  console.log(a);
-  page.querySelector(".profile__title").innerHTML = a;
-  let b = page.querySelector("#about-me-input").value;
-  console.log(b);
-  page.querySelector(".profile__subtitle").innerHTML = b;
-  popup.classList.remove("popup_opened");
-  console.log("Сохранение и закрытие попапа");
-}
-
-buttonSave.addEventListener("click", saveForm);
-
-let a = page.querySelector(".profile__title").innerHTML;
-
-page.querySelector("#user-name-input").value = a;
-
-let b = page.querySelector(".profile__subtitle").innerHTML;
-
-page.querySelector("#about-me-input").value = b;
-
-let formElement = page.querySelector(".popup__fields");
-let nameInput = formElement.querySelector("#user-name-input");
-let jobInput = formElement.querySelector("#about-me-input");
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-
-  page.querySelector("#user-name-input").value;
-  page.querySelector("#about-me-input").value;
-
-  page.querySelector(".profile__title").innerHTML;
-  page.querySelector(".profile__subtitle").innerHTML;
-
-  page.querySelector(".profile__title").innerHTML =
-    page.querySelector("#user-name-input").value;
-  page.querySelector(".profile__subtitle").innerHTML =
-    page.querySelector("#about-me-input").value;
-}
-
-formElement.addEventListener("submit", handleFormSubmit);
+saveForm.addEventListener("submit", saveFormSubmit);
