2b92a80d97b4b084c4c377a9dd0ecdd03a59ae1d
Marat Mustafin <mustafin.m-marat@yandex.ru>
2023-03-19 20:09:38 +0400

    Всё работает

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
-let page = document.querySelector(".page");
+const page = document.querySelector(".page");
 
-let profileEditButton = page.querySelector(".profile__edit-button");
+const profileEditButton = page.querySelector(".profile__edit-button");
 
-let buttonClose = page.querySelector(".popup__close");
+const buttonClose = page.querySelector(".popup__close");
 
-let popup = page.querySelector(".popup");
+const popup = page.querySelector(".popup");
 
-let titleCopy = page.querySelector(".profile__title");
+const titleCopy = page.querySelector(".profile__title");
 
-let subtitleCopy = page.querySelector(".profile__subtitle");
+const subtitleCopy = page.querySelector(".profile__subtitle");
 
-let nameInput = page.querySelector("#user-name-input");
+const nameInput = page.querySelector("#user-name-input");
 
-let aboutMeInput = page.querySelector("#about-me-input");
+const aboutMeInput = page.querySelector("#about-me-input");
 
-let formSave = page.querySelector(".popup__fields");
+const formSave = page.querySelector(".popup__fields");
 
 const place = page.querySelector(".places");
 
@@ -29,6 +29,16 @@ const nameImageInput = page.querySelector("#image-name-input");
 const imageInput = page.querySelector("#image-input");
 
 const fieldsImage = page.querySelector(".popup__fieldsImage");
+
+const placeImage = page.querySelector(".place__image");
+
+const imageScale = page.querySelector(".popup_image-scale");
+
+const imageView = page.querySelector(".popup__image-view");
+
+const imageTitle = page.querySelector(".popup__image-title");
+
+const imageClose = page.querySelector(".popup__close-image");
 
 const initialCards = [
   {
@@ -83,10 +93,10 @@ function clickCloseButtonAdd() {
 }
 
 function addImage(url) {
-  let placeTemplate = page
+  const placeTemplate = page
     .querySelector("#place-template")
     .content.cloneNode(true);
-  let placeElement = placeTemplate.querySelector(".place");
+  const placeElement = placeTemplate.querySelector(".place");
   placeElement.querySelector(".place__image").setAttribute("src", url.link);
   placeElement.querySelector(".place__image").setAttribute("alt", url.name);
   placeTemplate.querySelector(".place__title").textContent = url.name;
@@ -129,7 +139,31 @@ profileCloseButton.addEventListener("click", clickCloseButtonAdd);
 
 formSave.addEventListener("submit", saveFormSubmit);
 
-place.querySelector(".place__like").addEventListener("click", function (evt) {
-  evt.target.classList.toggle("place__like_active");
-});
 fieldsImage.addEventListener("submit", creatImageSubmit);
+
+function clickImageScale() {
+  imageScale.classList.add("popup_opened");
+}
+
+function closeImageView() {
+  imageScale.classList.remove("popup_opened");
+}
+
+imageClose.addEventListener("click", closeImageView);
+
+place.addEventListener("click", function (evt) {
+  if (evt.target.classList.contains("place__like")) {
+    evt.target.classList.toggle("place__like_active");
+  } else if (evt.target.classList.contains("place__remove")) {
+    evt.target.closest(".place").remove();
+  } else if (evt.target.classList.contains("place__image")) {
+    const placeTitle = evt.target
+      .closest(".place")
+      .querySelector(".place__title").textContent;
+    const placeName = evt.target.getAttribute("alt");
+    imageView.setAttribute("src", evt.target.src);
+    imageView.setAttribute("alt", placeName);
+    imageTitle.textContent = placeTitle;
+    clickImageScale();
+  }
+});
